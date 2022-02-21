73ee3c3f88b5e32067e2b1f12f1c505ec8c80bad
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2022-02-21 16:09:10 +0100

    [refactor] Add a container

--- sections/hero.php
+++ sections/hero.php
@@ -17,24 +17,26 @@ $hero = get_field("hero");
 	echo $hero["parallax"];
 	?>
 	<section class="introduction <?php if ($hero["effet_degrade"]) {echo "gradient";} ?>">
-		<?php
-		if (!empty($hero["logo"])):
-		?>
-			<a href="#top" class="introduction__logo-link">
-				<h1 class="introduction__logo-container">
-					<img src="<?php echo $hero["logo"]["url"]; ?>" alt="Wooops Agency" class="introduction__logo-image" />
-				</h1>
-			</a>
-		<?php
-		endif;
-		?>
-
-		<div class="introduction__description">
+		<div class="introduction__container">
 			<?php
-			if (!empty($hero["description"])):
-				echo $hero["description"];
+			if (!empty($hero["logo"])):
+			?>
+				<a href="#top" class="introduction__logo-link">
+					<h1 class="introduction__logo-container">
+						<img src="<?php echo $hero["logo"]["url"]; ?>" alt="Wooops Agency" class="introduction__logo-image" />
+					</h1>
+				</a>
+			<?php
 			endif;
 			?>
+
+			<div class="introduction__description">
+				<?php
+				if (!empty($hero["description"])):
+					echo $hero["description"];
+				endif;
+				?>
+			</div>
 		</div>
 	</section>
 </div>
